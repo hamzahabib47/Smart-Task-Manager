0321f14b03e930cd2124ab71b2e133ab13a3960d
Hamza <muhammadhamzahabib0@gmail.com>
2026-04-25 11:59:55 +0500

    Update build configs and dependencies

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.smart_time_manager_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
